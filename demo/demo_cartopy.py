928ec37494ea0314f4160e406f2ecf14c33c5ad9
Christopher Holmes <cdholmes@fsu.edu>
2018-10-08 15:36:54 -0400

    More Cartopy map examples
    Now includes examples for CONUS, Polar stereographic, and Robinson
    
    Use pseudo data generated by numpy rather than files from web
    
    Signed-off-by: Christopher Holmes <cdholmes@fsu.edu>

--- demo/demo_cartopy.py
+++ demo/demo_cartopy.py
@@ -5,33 +5,49 @@ Created on Sun Dec 13 20:48:05 2015
 @author: cdholmes
 """
 
-import iris
 import matplotlib.pyplot as plt
-
 import cartopy.crs as ccrs
+import cartopy.feature as cfeature 
+import numpy as np
+
+# Create artificial data
+n = 20
+lat  = np.linspace(-90,90,n+1)
+lon  = np.linspace(-180,180,2*n+1)
+lon2d, lat2d = np.meshgrid( lon,lat)
+data = np.exp( -lat2d**2/90**2 -lon2d**2/120**2 )
+
+# Open a figure
+plt.figure(1)
+plt.clf()
+
+# data coordinates: Use PlateCarree for data in lat-lon coordinates
+datacoord = ccrs.PlateCarree()
+
+# Map projection for Global 
+#ax = plt.axes(projection=ccrs.PlateCarree())
+#ax = plt.axes(projection=ccrs.Robinson())
+
+# Map projection for North Pole
+ax = plt.axes(projection=ccrs.NorthPolarStereo())
+ax.set_extent([-180, 180, 60, 90], ccrs.PlateCarree())
+
+# Map projection for CONUS 
+ax = plt.axes(projection=ccrs.LambertConformal())
+# Set lat-lon of map edges. These are reasonable for CONUS
+ax.set_extent([-120, -73, 25, 50], ccrs.PlateCarree())
 
 
-# load some sample iris data
-fname = iris.sample_data_path('rotated_pole.nc')
-temperature = iris.load_cube(fname)
 
-# iris comes complete with a method to put bounds on a simple point
-# coordinate. This is very useful...
-temperature.coord('grid_latitude').guess_bounds()
-temperature.coord('grid_longitude').guess_bounds()
+# Add your data
+plt.pcolormesh(lon, lat, data, transform=datacoord)
 
-# turn the iris Cube data structure into numpy arrays
-gridlons = temperature.coord('grid_longitude').contiguous_bounds()
-gridlats = temperature.coord('grid_latitude').contiguous_bounds()
-temperature = temperature.data
+# Add a colorbar 
+plt.colorbar()
 
-# set up a map
-ax = plt.axes(projection=ccrs.PlateCarree())
+# Add coasts and states; Use ‘110m’ or ‘50m’ resolution
+ax.coastlines(resolution='110m')
+ax.add_feature(cfeature.STATES.with_scale('110m'))
 
-# define the coordinate system that the grid lons and grid lats are on
-rotated_pole = ccrs.RotatedPole(pole_longitude=177.5, pole_latitude=37.5)
-plt.pcolormesh(gridlons, gridlats, temperature, transform=rotated_pole)
 
-ax.coastlines()
 
-plt.show()
