275fc1ea210d8894126b4a3623604b0c39ce53d9
Christopher Holmes <cdholmes@fsu.edu>
2019-07-25 17:00:26 -0600

    Error propagation. Initial commit.
    Signed-off-by: Christopher Holmes <cdholmes@fsu.edu>

---
+++ erroranalysis.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 13 21:23:38 2019
+
+@author: cdholmes
+"""
+import numpy as np
+
+def funcerror_uncorr(func,x0,xerr,**kwargs):
+    ''' Propagate uncertainties in function parameters to uncertainty in 
+    function value. The uncertainty will be evaluated at f(x0). The method uses 
+    a numerical estimate of the derivative of f(x0). For non-linear functions
+    the method is appropriate for small relative errors (i.e. small xerr/x0).
+    The method neglects possible error correlations between multiple parameters 
+    in x0.
+    The function variables must all be continuous variables 
+    (no categorical or discrete variables).
+    
+    Example. To find the uncertainty in f(x,y) at for values x=1, y=2, where 
+    the uncertainty (e.g. standard error) in x is 0.1 and the uncertainty in y 
+    is 0.3...
+    $ print( funcerr_uncorr( f, [1,2], [0.1,0.3] ) )
+    
+    input:
+        func : any function
+        x0   : a list or tuple containing function parameters
+        xerr : a lits or tuple containing uncertainty in each parameter
+    output:
+        ferr : the uncertainty in f(x0)
+    '''
+    
+    # Increment size for finite difference
+    # Numerical Methods in C recommends h * x, where x is the variable and 
+    # h = sqrt(machine precision). For a 64-bit (double) float, that implies 
+    # h = 2e-8. For a 32-bit (single) float, that implies h = 3e-4.
+    # We choose a larger value to for smaller roundoff error. Note 
+    # that very large h causes formula error where secant line is not parallel 
+    # to instantaneous slope.
+    h = 1e-7
+    h = 1e-3
+    
+    ferr = np.float64(0)
+        
+    for i,xi in enumerate(x0):
+        xplus     = x0.copy()
+        xminus    = x0.copy()
+        xplus[i]  = xplus[i]  + h*xi
+        xminus[i] = xminus[i] - h*xi
+        
+        # Centered finite difference estimate of derivative
+        dfdx = ( func(*xplus,**kwargs) - func(*xminus,**kwargs) ) / ( xplus[i]-xminus[i] )
+        
+        ferr += dfdx**2 * xerr[i]**2
+        
+    ferr = np.sqrt(ferr)
+        
+    return ferr
+   
