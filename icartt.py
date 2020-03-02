10e531d445aa8714fa00c4a92d83483b88f4b41f
Christopher Holmes <cdholmes@fsu.edu>
2020-03-02 17:34:36 -0500

    ICARTT files: support for missing data and scale factors.
    Signed-off-by: Christopher Holmes <cdholmes@fsu.edu>

--- icartt.py
+++ icartt.py
@@ -61,29 +61,53 @@ def read_icartt( files, usePickle=False, timeIndex=False ):
                 # Read name of the time variable
                 tname, tunit = [s.strip() for s in f.readline().split(',')[0:2]]
 
-                # Raise exception if the time unit is not seconds; may need to be handled differently below
+                # Raise exception if the time unit is not seconds;
+                # may need to be handled differently below
                 if (tunit in ['s','seconds','seconds (from midnight UTC)']):
                     # Use unit expected by pandas
                     tunit = 's'
                 else:
                     print(tunit)
-                    raise Exception( 'read_icartt: time expected in seconds (s); unit in file: ',tunit )
+                    raise Exception( 'read_icartt: time expected in seconds (s); '+
+                                     'unit in file: ',tunit )
 
+                # Number of dependent variables
+                nvar = int( f.readline() )
+
+                # Scale factor for dependent variables
+                scale = [ float(s) for s in f.readline().split(',') ]
+
+                # Missing value flag for dependent variables
+                naflag = [ s.strip() for s in f.readline().split(',') ]
+
+                # Dependent variable names from the next nvar lines
+                varnames = [ f.readline().split(',')[0] for v in range(nvar) ]
+
+                # Missing flags for each variable as a dict
+                nadict = { varnames[i]: naflag[i] for i in range(nvar) }
+            
             # Read data
-            obs = pd.read_csv(file,skiprows=nheader-1,na_values=' -99999')
+            obs = pd.read_csv(file, skiprows=nheader-1,
+                              na_values=nadict, skipinitialspace=True)
             
             # Catch missing data that are not reported as integers
             #obs[obs==-99999] = np.nan
-            
+
             # Strip whitespace from column names
-            obs.columns = obs.columns.str.strip()
-            
+            #obs.columns = obs.columns.str.strip()
+
+            # Apply scale factors
+            for i in range(len(scale)):
+                if (scale[i]!=1):
+                    obs[varnames[i]] *= scale[i]
+                
             # Rename selected variables
             obs.rename(columns={tname:'time'}, inplace=True)
         
             # Convert time to a datetime
             #obs.time = dt.datetime(year,month,day) + pd.TimedeltaIndex(obs.time,'s')
-            obs.time = pd.DatetimeIndex( pd.Timestamp(year=year,month=month,day=day) + pd.TimedeltaIndex(obs.time,tunit) )
+            obs.time = pd.DatetimeIndex( pd.Timestamp(year=year,month=month,day=day) +
+                                         pd.TimedeltaIndex(obs.time,tunit) )
         
             # Add flight number
             obs['file'] = n+1
